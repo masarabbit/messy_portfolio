8b67df139ac5d206862bf6252cf598089ca79d88
Masa Takeuchi <masahitoart@gmail.com>
2020-12-02 14:49:36 +0000

    test outerWidth

--- scripts/app.js
+++ scripts/app.js
@@ -131,13 +131,13 @@ function init() {
       biggerWidth = (window.innerHeight - 50) * ( e.target.width / e.target.height )
     } else {
       console.log('testB')
-      biggerHeight = (window.innerWidth - 40) * ( e.target.height / e.target.width )
-      biggerWidth = window.innerWidth - 40
+      biggerHeight = (window.outerWidth - 20) * ( e.target.height / e.target.width )
+      biggerWidth = window.outerWidth - 20
     }
     
     // setTimeout(()=>{
-    const newPos = (window.innerHeight - biggerHeight ) / 2 //* this need too be diferent?
-    const newLeft = (window.innerWidth - biggerWidth ) / 2
+    const newPos = (window.outerHeight - biggerHeight ) / 2 //* this need too be diferent?
+    const newLeft = (window.outerWidth - biggerWidth ) / 2
     // console.log(e.target)
     e.target.parentNode.style.transform = 'rotate(0deg)'
     e.target.parentNode.style.top = `${newPos}px`
