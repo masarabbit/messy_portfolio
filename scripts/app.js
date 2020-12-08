d9b016960b231bab76b663c8550ad0adb699a2f6
Masa Takeuchi <masahitoart@gmail.com>
2020-12-08 22:07:28 +0000

    z-index adjusted

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,17 @@
+
+//! setting z-index explicitly will overwrite anything done in css...
+
 function init() {
 
-  const body = document.querySelector('body')
+  // const body = document.querySelector('body')
   const imgSize = 30
-  let images = []
+  const images = []
   let screenAspect = 'horizontal'
   let aspectKey = 'vh'
   let biggerHeight
   let biggerWidth
+  let vertRatio = 1
+  let horiRatio = 1
 
   checkOrientation()
 
@@ -38,38 +43,53 @@ function init() {
     { height: 1414,width: 1000, image: 'animals.jpg' },
     { height: 750,width: 500, image: 'bunny_icecream2.jpg' }
   ]
+  
+  function setVertRatioAndHoriRatio(object){
+    
+    //! call something to set image's orientation?
+
+    if (object.height > object.width || object.height === object.width){
+      vertRatio = object.height / object.width
+      horiRatio = 1
+    } else {
+      horiRatio = object.width / object.height 
+      vertRatio = 1
+    }
+    
+    object.vertRatio = vertRatio
+    object.horiRatio = horiRatio
+  }
+
+  function setRandomAngleAndPosition(image,object) {
+    const randomAngle = Math.floor(Math.random() * 360)
+    const randomTopPos = `${Math.floor(Math.random() * 70)}%`
+    const randomLeftPos = `${Math.floor(Math.random() * 70)}%`
+
+    image.style.height = `${imgSize * vertRatio + aspectKey}`
+    image.style.width = `${imgSize * horiRatio + aspectKey}`
+    image.style.top = randomTopPos
+    image.style.left = randomLeftPos
+    image.style.transform = 'rotate(' + randomAngle + 'deg)'
+    
+    object.angle = randomAngle
+    object.topPos = randomTopPos
+    object.leftPos = randomLeftPos
+    image.classList.add('z1')
+  }
+
 
   function createImage(object){
     const portfolio = document.querySelector('.portfolio')
     const newImage = document.createElement('div')
-    const randomAngle = Math.floor(Math.random() * 360)
-    const randomTopPos = `${Math.floor(Math.random() * 70)}%`
-    const randomLeftPos = `${Math.floor(Math.random() * 70)}%`
-    let vertRatio = 1
-    let horiRatio = 1
-    
-    if (object.height > object.width || object.height === object.width){
-      vertRatio = object.height / object.width
-    } else {
-      horiRatio = object.width / object.height 
-    }
 
+    setVertRatioAndHoriRatio(object)
+    
     newImage.classList.add('image_thumb')
     newImage.innerHTML = `<img src = "./assets/${object.image}" alt = "${object.image.replace('_',' ').replace('.jpg','').replace('.gif','').replace('.png','')}">`
-
-    newImage.style.height = `${imgSize * vertRatio + aspectKey}`
-    newImage.style.width = `${imgSize * horiRatio + aspectKey}`
-    newImage.style.top = randomTopPos
-    newImage.style.left = randomLeftPos
-    newImage.style.transform = 'rotate(' + randomAngle + 'deg)'
-    portfolio.appendChild(newImage)
     
-    object.vertRatio = vertRatio
-    object.horiRatio = horiRatio
-    object.angle = randomAngle
-    object.topPos = randomTopPos
-    object.leftPos = randomLeftPos
-
+    setRandomAngleAndPosition(newImage,object)
+    
+    portfolio.appendChild(newImage)
     images.push(newImage)
   }
 
@@ -83,24 +103,34 @@ function init() {
       image.addEventListener('click',displayImage)
     })
   }
-  
-  
-  function reset(){
 
-    images.forEach(image =>{
-      image.removeEventListener('click',displayImage)
-    })
-    
-    images = []
-
-    body.innerHTML = `
-    <div class ='wrapper'>
-      <div class="portfolio"></div>
-    </div>`
-
-    setUp()
+  function reposition() {
     checkOrientation()
+
+    for (let i = 0; i < imageArray.length; i++){
+      setVertRatioAndHoriRatio(imageArray[i])
+      setRandomAngleAndPosition(images[i],imageArray[i])
+      console.log(images[i])
+    }
   }
+  
+  
+  // function reset(){
+
+  //   images.forEach(image =>{
+  //     image.removeEventListener('click',displayImage)
+  //   })
+    
+  //   images = []
+
+  //   body.innerHTML = `
+  //   <div class ='wrapper'>
+  //     <div class="portfolio"></div>
+  //   </div>`
+
+  //   setUp()
+  //   checkOrientation()
+  // }
 
 
   function displayImage(e){    
@@ -111,7 +141,7 @@ function init() {
       hidePrevImage(prevImage)
     }
 
-    if (screenAspect === 'horizontal'){
+    if (screenAspect === 'horizontal'){ //! this should also check for image ratio
       // console.log('test')
       biggerHeight = window.innerHeight - 50
       biggerWidth = (window.innerHeight - 50) * ( e.target.width / e.target.height )
@@ -141,13 +171,20 @@ function init() {
     e.target.parentNode.addEventListener('click',displayImage)
 
     const index = images.indexOf(e.target.parentNode)
-  
-    images[index].style.transform = `rotate(${imageArray[index].angle}deg)`
-    images[index].style.top = `${imageArray[index].topPos}`
-    images[index].style.left = `${imageArray[index].leftPos}`
-    images[index].style.height = `${imgSize * imageArray[index].vertRatio + aspectKey}`
-    images[index].style.width = `${imgSize * imageArray[index].horiRatio + aspectKey}`
+
+    if (Math.floor(Math.random() * 2) === 1){
+      setVertRatioAndHoriRatio(imageArray[index])
+      setRandomAngleAndPosition(e.target.parentNode,imageArray[index])
+    } else {  //* randomly switches between random position and previous position
+      images[index].style.transform = `rotate(${imageArray[index].angle}deg)`
+      images[index].style.top = `${imageArray[index].topPos}`
+      images[index].style.left = `${imageArray[index].leftPos}`
+      images[index].style.height = `${imgSize * imageArray[index].vertRatio + aspectKey}`
+      images[index].style.width = `${imgSize * imageArray[index].horiRatio + aspectKey}`
+    }
+
     e.target.parentNode.classList.remove('pick')
+    e.target.parentNode.classList.remove('z1')
   }
   
 
@@ -156,16 +193,24 @@ function init() {
     prevImage.addEventListener('click',displayImage)
 
     const index = images.indexOf(prevImage)
-  
-    prevImage.style.transform = `rotate(${imageArray[index].angle}deg)`
-    prevImage.style.top = `${imageArray[index].topPos}`
-    prevImage.style.left = `${imageArray[index].leftPos}`
-    prevImage.style.height = `${imgSize * imageArray[index].vertRatio + aspectKey}`
-    prevImage.style.width = `${imgSize * imageArray[index].horiRatio + aspectKey}`
+
+    if (Math.floor(Math.random() * 2) === 1){
+      setVertRatioAndHoriRatio(imageArray[index])
+      setRandomAngleAndPosition(prevImage,imageArray[index])
+    } else {
+      prevImage.style.transform = `rotate(${imageArray[index].angle}deg)`
+      prevImage.style.top = `${imageArray[index].topPos}`
+      prevImage.style.left = `${imageArray[index].leftPos}`
+      prevImage.style.height = `${imgSize * imageArray[index].vertRatio + aspectKey}`
+      prevImage.style.width = `${imgSize * imageArray[index].horiRatio + aspectKey}`
+    }
+    
     prevImage.classList.remove('pick')
+    prevImage.classList.remove('z1')
   }
 
-  window.addEventListener('resize', reset)
+  // window.addEventListener('resize', reset)
+  window.addEventListener('resize', reposition)
   
   setUp()
 }
