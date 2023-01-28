3d6bd7e2c5c67792bfd163ef93474a962e3b6925
masarabbit <masahitoart@gmail.com>
2023-01-28 15:22:20 +0000

    add drag

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,9 +1,8 @@
 
 function init() {
-
-  // TODO add drag?
   // TODO add reshuffle
   // TODO add shift to grid?
+  // TODO refactor inconsistency (img vs card)
   
   const imgData = [
     { h: 200, w: 200, img: 'mochimochiusagi.gif' },
@@ -24,7 +23,6 @@ function init() {
     stackButton: document.querySelector('.stack')
   }
 
-
   const setting = {
     imgSize: 30,
     images: [],
@@ -37,18 +35,19 @@ function init() {
     horiRatio: 1,
     imgIndex: null,
     stack: false,
+    offsetX: 20,
+    offsetY: 20,
+    isDragging: false,
   }
 
-  const checkOrientation = () =>{
-    setting.screenAspect = window.innerHeight > window.innerWidth ? 'vertical' : 'horizontal'
-    setting.aspectKey = setting.screenAspect === 'vertical' ? 'vw' : 'vh'
-  }
-  
   const isActive = target => target.classList.contains('pick')
+  const withinBuffer = ({ a, b, buffer }) => Math.abs(a - b) < buffer
   const randomPos = () => `${Math.floor(Math.random() * 70)}%`
   const randomAngle = () => Math.floor(Math.random() * 360)
   const isNum = x => typeof x === 'number'
   const px = num => `${num}px`
+  const client = (e, type) => e.type[0] === 'm' ? e[`client${type}`] : e.touches[0][`client${type}`]
+  const roundedClient = (e, type) => Math.round(client(e, type))
 
   const setProperty = (target, property, value, prefix) => {
     target.style.setProperty(`--${prefix ? `${prefix}-` : ''}${property}`, value)
@@ -64,6 +63,76 @@ function init() {
     if (isNum(delay)) setProperty(target, 'delay', `${delay}s`, prefix)
   }
 
+  const addEvents = (target, event, action, array) => {
+    array.forEach(a => event === 'remove' ? target.removeEventListener(a, action) : target.addEventListener(a, action))
+  }
+  const mouse = {
+    up: (t, e, a) => addEvents(t, e, a, ['mouseup', 'touchend']),
+    move: (t, e, a) => addEvents(t, e, a, ['mousemove', 'touchmove']),
+    down: (t, e, a) => addEvents(t, e, a, ['mousedown', 'touchstart']),
+    enter: (t, e, a) => addEvents(t, e, a, ['mouseenter', 'touchstart']),
+    leave: (t, e, a) => addEvents(t, e, a, ['mouseleave', 'touchmove'])
+  }
+
+  const drag = (e, target, pos, x, y) => {
+    if (e.type[0] === 'm') e.preventDefault()  
+    pos.a = pos.c - x
+    pos.b = pos.d - y
+    const newX = target.offsetLeft - pos.a
+    const newY = target.offsetTop - pos.b
+
+    if (!withinBuffer({ a:newX, b: target.offsetLeft, buffer: 1 })) {
+      setting.isDragging = true
+      setProperties({
+        target: target,
+        x: px(newX), y: px(newY),
+      })
+    }
+  }
+
+
+  const addDragAction = target =>{
+    const pos = { a: 0, b: 0, c: 0, d: 0 }
+    
+    const onGrab = e =>{
+      if (!setting.stack) {
+        pos.c = roundedClient(e, 'X')
+        pos.d = roundedClient(e, 'Y')  
+        mouse.up(document, 'add', onLetGo)
+        mouse.move(document, 'add', onDrag)
+        target.classList.add('drag')
+      }
+    }
+    const onDrag = e =>{
+      const x = roundedClient(e, 'X')
+      const y = roundedClient(e, 'Y')
+      drag(e, target, pos, x, y)
+      pos.c = x
+      pos.d = y
+    }
+    const onLetGo = () => {
+      mouse.up(document, 'remove', onLetGo)
+      mouse.move(document,'remove', onDrag)
+      target.classList.remove('drag')
+      const newX = target.offsetLeft - pos.a
+      const newY = target.offsetTop - pos.b
+      setProperties({
+        target: target,
+        x: px(newX), y: px(newY),
+      })
+      // need to delay this so click action doesn't trigger straight after dragging
+      setTimeout(()=> setting.isDragging = false)
+    }
+    mouse.down(target,'add', onGrab)
+  }
+
+  const checkOrientation = () =>{
+    setting.screenAspect = window.innerHeight > window.innerWidth ? 'vertical' : 'horizontal'
+    setting.aspectKey = setting.screenAspect === 'vertical' ? 'vw' : 'vh'
+  }
+  
+
+
   const alt = imgName => {
     let alt = imgName
     ;['.jpg', '.gif', '.png'].forEach(l => alt = alt.replace(l, ''))
@@ -77,17 +146,16 @@ function init() {
     setting.horiRatio = isVert ? 1 : w / h
   }
 
-  const setRandomAngleAndPosition = (img, obj) => {
+  const setRandomAngleAndPosition = img => {
     const { imgSize, vertRatio, horiRatio, aspectKey, screenAspect } = setting
     const imgHeight = imgSize * vertRatio
     const imgWidth = imgSize * horiRatio
 
     setProperties({
       target: img,
-      h: imgHeight + aspectKey,
       w: imgWidth + aspectKey,
-      y: randomPos(),
-      x: randomPos(),
+      h: imgHeight + aspectKey,
+      x: randomPos(), y: randomPos(),
       deg: randomAngle(),
     })
 
@@ -108,8 +176,8 @@ function init() {
       target: img,
       w: px(setting.greaterW),
       h: px(setting.greaterH),
-      y: px((h - setting.greaterH) / 2),
       x: px((w - setting.greaterW) / 2),
+      y: px((h - setting.greaterH) / 2),
       deg: 0,
       prefix: 'display'
     })
@@ -121,7 +189,7 @@ function init() {
     newImg.classList.add('card')
     newImg.dataset.index = index
     newImg.innerHTML = `<img data-index="${index}" src= "./assets/${obj.img}" alt="${alt(obj.img)}">`
-    setRandomAngleAndPosition(newImg, obj)
+    setRandomAngleAndPosition(newImg)
     
     elements.portfolio.appendChild(newImg)
     setting.images.push(newImg)
@@ -139,33 +207,25 @@ function init() {
   const setUp = () => {
     imgData.forEach((img, i) => createImage(img, i))
     setting.sortedImages = [...setting.images]
-    setting.images.forEach(img => img.addEventListener('click', triggerCardAction))
-    ;['mouseenter', 'touchstart'].forEach(action => {
-      setting.images.forEach(img => img.addEventListener(action, () => {
-        peek(img, 'add')
-      }))
-    })
-
-    ;['mouseleave', 'touchend'].forEach(action => {
-      setting.images.forEach(img => img.addEventListener(action, () => {
-        peek(img, 'remove')
-      }))
+    setting.images.forEach(img => {
+      img.addEventListener('click', triggerCardAction)
+      addDragAction(img)
+      mouse.enter(img, 'add', ()=> peek(img, 'add'))
+      mouse.leave(img, 'add', ()=> peek(img, 'remove'))
     })
   }
 
-  const positionSortedCards = () => {
+  const positionStackedCards = () => {
     const { innerHeight: h, innerWidth: w } = window
     const defaultX = (w - 200) / 2
     const defaultY = (h - 300) / 2
-    const x = 20
-    const y = 20
     let z = 900
     
     setting.sortedImages.forEach((card, i) => {
       setProperties({
         target: card,
-        x: px(defaultX - x * (setting.images.length - i)),
-        y: px(defaultY - y * (setting.images.length - i)),
+        x: px(defaultX - setting.offsetX * (setting.images.length - i)),
+        y: px(defaultY - setting.offsetY * (setting.images.length - i)),
         deg: 0,
         z: z++,
         prefix: 'stack'
@@ -181,23 +241,25 @@ function init() {
       setRandomAngleAndPosition(setting.images[i], img)
     })
 
-    positionSortedCards()
+    positionStackedCards()
   }
 
   const hideOrDisplayImage = e => {
-    const { images, imgIndex } = setting
-    setting.images.forEach(card => card.classList.remove('pick'))
-    e.target.parentNode.classList.add('pick')
-
-    if (isNum(imgIndex) && isActive(images[imgIndex])) {
-      hideImage(imgIndex)
-      e.target.parentNode.classList.remove('pick')
-      setting.imgIndex = null
+    if (!setting.isDragging) {
+      const { images, imgIndex } = setting
+      setting.images.forEach(card => card.classList.remove('pick'))
+      e.target.parentNode.classList.add('pick')
+  
+      if (isNum(imgIndex) && isActive(images[imgIndex])) {
+        hideImage(imgIndex)
+        e.target.parentNode.classList.remove('pick')
+        setting.imgIndex = null
+      }
+  
+      if (isActive(e.target.parentNode)) {
+        setting.imgIndex = +e.target.dataset.index
+      } 
     }
-
-    if (isActive(e.target.parentNode)) {
-      setting.imgIndex = +e.target.dataset.index
-    } 
   }
 
   const moveStackedCards = e => {
@@ -209,11 +271,10 @@ function init() {
     if (selectedCardIndex === setting.sortedImages.length - 1) {
       const otherCards = setting.sortedImages.filter(card => card.dataset.index !== e.target.dataset.index)
       setting.sortedImages = [selectedCard,...otherCards]
-      positionSortedCards()
+      positionStackedCards()
       setProperties({
         target: selectedCard,
-        x: px(left),
-        y: px(top),
+        x: px(left), y: px(top),
         z: 900 + setting.images.length,
         delay: 0,
         prefix: 'prev-stack'
@@ -228,14 +289,14 @@ function init() {
       const cardsToMove = setting.sortedImages.filter((_card, i) => i > selectedCardIndex)
       const otherCards = setting.sortedImages.filter((_card, i) => i <= selectedCardIndex)
       setting.sortedImages = [...cardsToMove,...otherCards]
-      positionSortedCards()
+      positionStackedCards()
       cardsToMove.forEach((card, i) => {
         const offset = i + 1
         setProperties({
           target: card,
-          x: px(left + (offset * 20)),
-          y: px(top + (offset * 20)),
-          z: 900 - offset,
+          x: px(left + (offset * setting.offsetX)),
+          y: px(top + (offset * setting.offsetY)),
+          z: 9900 + offset,
           delay: offset * 0.05,
           prefix: 'prev-stack'
         })
@@ -268,9 +329,8 @@ function init() {
 
   elements.stackButton.addEventListener('click', ()=> {
     setting.stack = !setting.stack
-    setting.sortedImages.forEach(card => {
-      card.classList.toggle('stack')
-    })
+    setting.sortedImages.forEach(card => card.classList.toggle('stack'))
+    setting.images.forEach(card => card.classList.remove('pick'))
   })
   
 }
